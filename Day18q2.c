06687ec8470c0263f51b2878c950e65be3040914
Anvesha <anveshasep06@gmail.com>
2025-11-06 14:36:03 +0530

    Day18q2.c

--- Day18q2.c
+++ Day18q2.c
@@ -19,12 +19,10 @@ int main() {
     int a, b, i, hcf;
 
     printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-
-    // Find the smaller number
+    scanf("%d %d", &a, &b)
     int min = (a < b) ? a : b;
 
-    // Find HCF by checking divisibility
+    
     for (i = 1; i <= min; i++) {
         if (a % i == 0 && b % i == 0) {
             hcf = i;
