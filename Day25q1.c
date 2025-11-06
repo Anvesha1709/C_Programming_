5076b950e753cf6ca963cfafa5775736d7e4ad68
Anvesha <anveshasep06@gmail.com>
2025-11-06 14:27:01 +0530

    Day25q1.c

--- Day25q1.c
+++ Day25q1.c
@@ -22,13 +22,13 @@ Output 1:
 int main() {
     int i, j;
 
-    // Outer loop for rows
+
     for (i = 5; i >= 1; i--) {
-        // Inner loop for numbers
+        
         for (j = i; j <= 5; j++) {
             printf("%d", j);
         }
-        printf("\n"); // Move to next line after each row
+        printf("\n"); 
     }
 
     return 0;
