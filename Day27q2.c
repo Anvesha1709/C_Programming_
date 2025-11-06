059f2666fdc247c798e1fccc36852fccac79ae60
Anvesha <anveshasep06@gmail.com>
2025-11-06 14:23:25 +0530

    Day27q2.c

--- Day27q2.c
+++ Day27q2.c
@@ -20,28 +20,26 @@ Pattern with layers of stars as shown.
 #include <stdio.h>
 
 int main() {
-    int i, j, n = 4;  // number of rows in the upper half
-
-    // Upper half
+    int i, j, n = 4;  
     for (i = 1; i <= n; i++) {
-        // Print spaces
+        
         for (j = i; j < n; j++) {
             printf(" ");
         }
-        // Print stars
+        
         for (j = 1; j <= (2 * i - 1); j++) {
             printf("*");
         }
         printf("\n");
     }
 
-    // Lower half
+    
     for (i = n - 1; i >= 1; i--) {
-        // Print spaces
+      
         for (j = n; j > i; j--) {
             printf(" ");
         }
-        // Print stars
+      
         for (j = 1; j <= (2 * i - 1); j++) {
             printf("*");
         }
