c25e9a4e01a794d4960420543ff2ca0f6f52fe35
Anvesha <anveshasep06@gmail.com>
2025-11-06 14:24:09 +0530

    Day27q1.c

--- Day27q1.c
+++ Day27q1.c
@@ -28,20 +28,20 @@ Output 1:
 #include <stdio.h>
 
 int main() {
-    int i, j, n = 5;  // n is the number of rows in the upper half
+    int i, j, n = 5;  
 
-    // Upper half of the pattern
+    
     for (i = 1; i <= n; i++) {
-        // print stars
+        
         for (j = 1; j <= (2 * i - 1); j++) {
             printf("*");
         }
         printf("\n");
     }
 
-    // Lower half of the pattern
+    
     for (i = n - 1; i >= 1; i--) {
-        // print stars
+        
         for (j = 1; j <= (2 * i - 1); j++) {
             printf("*");
         }
