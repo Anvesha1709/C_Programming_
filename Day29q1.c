dfbd4e8084af4b8f382c5e7b0d022f2ea44ec316
Anvesha <anveshasep06@gmail.com>
2025-11-06 14:18:06 +0530

    Day29q1.c

--- Day29q1.c
+++ Day29q1.c
@@ -20,24 +20,23 @@ Output 2:
 int main() {
     int n, i, sum = 0;
 
-    // Input number of elements
+    
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    int arr[n];  // declare array
+    int arr[n];  
 
-    // Input array elements
+    
     printf("Enter %d elements:\n", n);
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Calculate sum
     for (i = 0; i < n; i++) {
         sum += arr[i];
     }
 
-    // Output result
+    
     printf("%d\n", sum);
 
     return 0;
