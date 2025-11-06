d3366aebf70cddbde70200e7df4816061f4f8493
Anvesha <anveshasep06@gmail.com>
2025-11-06 14:31:22 +0530

    Day21q2.c

--- Day21q2.c
+++ Day21q2.c
@@ -19,32 +19,29 @@ Output 2:
 int main() {
     int num, firstDigit, lastDigit, digits, swappedNum;
 
-    // Input number
+
     scanf("%d", &num);
 
-    // Find last digit
+    
     lastDigit = num % 10;
 
-    // Find total digits
+
     digits = (int)log10(num);
 
-    // Find first digit
+    
     firstDigit = num / pow(10, digits);
 
-    // If number has only one digit
+    
     if (num < 10) {
         printf("%d", num);
         return 0;
     }
 
-    // Remove first and last digits, keep middle part
+    
     int middle = num % (int)pow(10, digits);
     middle = middle / 10;
-
-    // Construct the new number
     swappedNum = lastDigit * pow(10, digits) + middle * 10 + firstDigit;
 
-    // Output
     printf("%d", swappedNum);
 
     return 0;
